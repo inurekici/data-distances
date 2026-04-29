a179da7873993ecf89a061b439f015d68e87c134
ilayda ekici <ilos@MacBookAir.fritz.box>
2026-04-29 09:22:17 +0200

    feat(distances): add euclidean distance function

--- utils/distances.py
+++ utils/distances.py
@@ -2,3 +2,7 @@ def manhattan (point1, point2):
     d_x = abs(point2[0] - point1[0])
     d_y = abs(point2[1] - point1[1])
     return d_x + d_y
+
+
+def euclidean(a, b):
+    return ((b[0] - a[0])**2 + (b[1] - a[1])**2) ** 0.5
